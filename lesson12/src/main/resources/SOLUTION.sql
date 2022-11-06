4b794745456f43b9774426559325cf1e9f106ca3
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-11-06 19:16:18 +0400

    SQL task

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,3 @@
-DELETE FROM ;
 DELETE FROM student WHERE id IN (SELECT s.id FROM student AS s JOIN mark AS m ON s.id = m.student_id WHERE m.mark < 4 GROUP BY s.id);
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
